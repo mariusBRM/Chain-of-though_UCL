709c8848247b9c91d728f12ca22e3fde85b580ec
mariusBRM <marius.reymauzaize@gmail.com>
2023-06-09 10:46:05 +0100

    [UPDATE] train function and dataloading function

--- src/santaC.py
+++ src/santaC.py
@@ -2,28 +2,35 @@ import torch
 import re
 import torch.nn as nn
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+import numpy as np
+import random
+from torch.utils.data import DataLoader
+from tokenizer import *
 
+
+#################################
+#       Define SantaCoder       #
+#################################
 
 class MySantaCoder(nn.Module):
-    def __init__(self, model_path_to_hub):
+    def __init__(self):
         super(MySantaCoder, self).__init__()
-        # self.checkpoint = "bigcode/santacoder"
-        self.checkpoint = model_path_to_hub
+        self.checkpoint = "bigcode/santacoder"
+        # self.checkpoint = model_path_to_hub
         self.model = AutoModelForCausalLM.from_pretrained(self.checkpoint, trust_remote_code=True)
         self.tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
-        self.max_new_tokens=512
+        self.max_new_tokens = 2048
+        self.num_solution_to_generate = 200
         self.stop_words=["\nclass", "\ndef","\nassert", '\n"""', "\nprint", "\n<|"]
 
-        self.generation_config = GenerationConfig(
-            do_sample=True, 
-            top_k=5,
+        self.generation_config = GenerationConfig( 
+            num_return_sequences = self.num_solution_to_generate,
             max_length = self.max_new_tokens,
             eos_token_id=self.model.generation_config.eos_token_id,
             bos_token_id=self.model.generation_config.bos_token_id
             )
     
-    def forward(self, input):
-        input_ids = input['input_ids'].unsqueeze(0)
+    def forward(self, input_ids):
         outputs = self.model.generate(input_ids, self.generation_config)
         return outputs
     
@@ -43,26 +50,150 @@ class MySantaCoder(nn.Module):
         result = list_blocks[0] + fill_word + list_blocks[1]
         return result
     
+#################################
+#       Define Training         #
+#################################
 
-def train(model, dataloader, nm_epochs):
+def loss_fn(output, target):
+    return nn.BCELoss()(output, target)
 
+def validation(validation_loader, model):
+    """
+    This function evaluate the model on the validation data
+    """
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.to(device)
-    
+
     fin_targets=[]
     fin_outputs=[]
     running_loss = 0.0
 
     with torch.no_grad():
-        for _,data in enumerate(dataloader, 0):
+        for _, data in enumerate(validation_loader, 0):
 
-            input = data.to(device, dtype = torch.long)
-            labels = input['labels']
-            # output
-            output = model.forward(input)
-            # loss
+            ids = data['input_ids'].to(device, dtype = torch.long)
+            targets = data['labels'].to(device, dtype = torch.long)
+            
+            # forward
+            output = model.forward(ids)
+            # evaluate the loss
+            loss = loss_fn(output, targets)
+ 
+            # adding to list
+            fin_targets.extend(targets.cpu().detach().numpy().tolist())
+            fin_outputs.extend(output.cpu().detach().numpy().tolist())
+
+            # add the loss to the running loss
+            running_loss+=loss.item()
+
+    return fin_outputs, fin_targets, running_loss/len(validation_loader)
+
+def training_model(nb_epochs, train_dataloader, val_dataloader, patience):
+    """
+    This function trains the model on training data
+    """
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = MySantaCoder()
+    model.to(device)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-5)
+    best_val_loss = np.inf
+    # keep track of the performances
+    summary = []
+
+
+    for epoch in range(nb_epochs):
+            # dict containing the information
+        report_epoch = {
+                'epoch': epoch+1,
+                'training_loss': 0.0,
+                'valid_loss':0.0
+            }
+        model.train()
+        running_loss = 0.0
+
+        for i, data in enumerate(train_dataloader, 0):
+
+            ids = data['input_ids'].to(device, dtype = torch.long)
+            labels = data['labels'].to(device, dtype = torch.long)
+            
+             # initialize the optimizer
+            optimizer.zero_grad()
+            #forward inputs
+            output = model.forward(ids) 
+            # define the loss
+            loss = loss_fn(output, labels)
+            # backpropagate
+            loss.backward()
+            optimizer.step()
+            # add the loss to the running loss
+            running_loss+=loss.item()
+            
+            print('\rEpoch: {}\tbatch: {}\tLoss =  {:.3f}'.format(epoch+1, i, loss), end="")
+
+        running_loss = running_loss / len(train_dataloader)
+        report_epoch['training_loss'] = running_loss
+        print("\n")
+        # validation
+        model.eval()
+        with torch.no_grad():
+
+            outputs, targets, val_loss = validation(validation_loader=val_dataloader, model= model)
+
+            report_epoch['valid_loss'] = val_loss
+            # set to evaluate potential metrics...
+
+            print(f"Epoch {epoch+1}: train CE loss = {running_loss}", f"|| Valid: CE loss = {val_loss}")
             
 
+        # early-stopping
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            dict_model = model.state_dict()
+            pat = 0
+        else:
+            pat += 1
+            print("pat ", pat)
+            if pat == patience:
+                print("Early Stopping: Validation Loss did not decrease for", patience, "epochs.")
+                break
+        
+        # save model each epoch
+        torch.save(model.state_dict(), 'epoch'+str(epoch+1)+'.pt')
 
+        print("\n")
+        # add performances of the epoch to the overall summary
+        summary.append(report_epoch)
 
-    return None
+    torch.save(dict_model, 'Fine_Tuned_SantaCoder.pt')
+    
+    return summary
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+if __name__=="__name__":
+    
+    # load the data
+    train_data, val_data = ''
+    PATH_TO_HUB = 'bigcode/santacoder'
+
+    g = torch.Generator()
+    g.manual_seed(0)
+
+    # Tokenizing datasets
+    trainset = MyTokenizer(train_data, PATH_TO_HUB)
+    valset = MyTokenizer(val_data, PATH_TO_HUB)
+
+    # Create dataloader
+    batch_size = 1
+    num_workers = 0
+
+    trainloader = dataloading(train_data, PATH_TO_HUB, batch_size, num_workers, g, seed_worker)
+    validloader = dataloading(val_data, PATH_TO_HUB, batch_size, num_workers, g, seed_worker)
+
+    # set training parameters ( look out how to enhance this)
+    nm_epochs = 5
+    patience = 2
+    summary = training_model(nm_epochs, trainloader, validloader, patience)
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 # Custom dataset
 class MyTokenizer(Dataset):
@@ -32,3 +32,17 @@ class MyTokenizer(Dataset):
             'labels': torch.tensor(label, dtype=torch.long)
         }
     
+
+def dataloading(data, path_to_hub, batch_size, num_workers, seed_worker):
+    # create dataset out of dataframe
+    dataset = MyTokenizer(data, path_to_hub)
+
+    # create dataloader
+    dataloader = = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        worker_init_fn=seed_worker,
+        generator=g,
+        )
+
